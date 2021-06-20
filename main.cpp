de667978c878f7cc1042ecf878c81d2456a2c8a1
Yurok <predbannikov.yurii@gmail.com>
2021-06-20 15:16:02 +0300

    add smth fix and add aliase (using)

--- main.cpp
+++ main.cpp
@@ -2,16 +2,17 @@
 #include <vector>
 #include <stack>
 
-int counter_isle(std::vector<std::vector<int > > world){
+using world_t = std::vector<std::vector<int>>;
+
+int counter_isle(world_t world){
     int counter = 0;
+    std::stack<std::pair<int,int>>stack;
     for(int i = 0; i < world.size(); i++) {
         for(int j = 0; j < world[i].size(); j++) {
             if(world[i][j] == 1) {
                 counter++;
-                std::stack<std::pair<int,int>>stack;
                 stack.push({i, j});
                 while(!stack.empty()) {
-                    auto pair = stack.top();
                     auto [x,y] = stack.top();
                     stack.pop();
                     world[x][y] = 0;
@@ -21,7 +22,7 @@ int counter_isle(std::vector<std::vector<int > > world){
                     if(x > 0 && world[x-1][y] == 1) {
                         stack.push({x-1, y});
                     }
-                    if(y < world[i].size()-1 && world[x][y+1] == 1) {
+                    if(y < world[x].size()-1 && world[x][y+1] == 1) {
                         stack.push({x, y+1});
                     }
                     if(y > 0 && world[x][y-1] == 1) {
@@ -36,14 +37,14 @@ int counter_isle(std::vector<std::vector<int > > world){
 
 int main()
 {
-    std::vector<std::vector<int > > srcWorld {
-        {0, 0, 0, 1, 1, 1, 1, 1, 0},
+    world_t srcWorld {
+        {1, 0, 0, 1, 1, 1, 1, 1, 1},
         {0, 0, 0, 1, 0, 1, 0, 0, 0},
-        {0, 1, 1, 1, 1, 1, 0, 0, 0},
+        {0, 1, 1, 0, 1, 1, 0, 0, 0},
         {0, 1, 0, 1, 0, 0, 0, 0, 0},
         {0, 0, 0, 1, 0, 0, 0, 0, 0},
         {0, 1, 1, 1, 0, 0, 0, 0, 1},
-        {0, 1, 1, 0, 0, 0, 0, 1, 1},
+        {1, 1, 1, 0, 0, 0, 0, 1, 1},
     };
     std::cout << "count isle = " << counter_isle(srcWorld) << std::endl;
     return 0;
